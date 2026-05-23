dd5a5d183c10980ff2b4780298ff9f4b9dcd4f28
Cozy <1365055997@qq.com>
2026-05-24 03:55:42 +0800

    Remove build directory

--- build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Point_FILE E:/Qt/QtProject/Point/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/Point.exe)
-set(__QT_DEPLOY_TARGET_Point_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Point_RUNTIME_DLLS E:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
